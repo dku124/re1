012a1fd12101350f56102413ab3a4a5c9bc2b1b5
dku124 <duongkhanhuyen124@gmail.com>
2024-01-10 16:22:46 +0700

    create pages

---
+++ src/pages/AllFoods.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AllFoods() {
+  return (
+    <div>
+      AllFoods
+    </div>
+  )
+}
+
+export default AllFoods
---
+++ src/pages/FoodDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FoodDetail() {
+  return (
+    <div>
+      FoodDetail
+    </div>
+  )
+}
+
+export default FoodDetail
